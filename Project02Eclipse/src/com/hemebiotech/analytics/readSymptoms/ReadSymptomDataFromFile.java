cca447a203fc23ab92380a4f5f643e3f14de9867
laurentfrederick <laurent.frederick@icloud.com>
2021-06-14 12:02:36 +0200

    fix read symptoms and create a package read

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/readSymptoms/ReadSymptomDataFromFile.java
@@ -0,0 +1,57 @@
+package com.hemebiotech.analytics.readSymptoms;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Simple brute force implementation
+ *
+ */
+public class ReadSymptomDataFromFile implements ISymptomReader {
+
+	private FileReader fileRead;
+
+	/**
+	 *
+	 * @param filePath a full or partial path to file with symptom strings in it, one per line
+	 */
+	public ReadSymptomDataFromFile(String filePath) {
+		try {
+			this.fileRead = new FileReader(filePath);
+		} catch (FileNotFoundException e) {
+			System.err.println("File : " + filePath + " has been not found");
+		}
+	}
+
+	@Override
+	public List<String> getSymptoms() {
+		ArrayList<String> result = new ArrayList<>();
+		BufferedReader reader = new BufferedReader(this.fileRead);
+
+		String line = null;
+		try {
+			line = reader.readLine();
+			while (line != null) {
+				result.add(line);
+				line = reader.readLine();
+			}
+		} catch (IOException e) {
+			System.err.println("Error of reading the line : " + line + "\"");
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				System.err.println("Error while bufferReader's closed");
+			}
+		}
+
+		return result;
+	}
+
+
+
+}
